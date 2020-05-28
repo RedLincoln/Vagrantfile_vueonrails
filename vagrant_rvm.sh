a84ece5166d89016449623f7abce494ab1bea257
RedLincoln <miguel_lcp@hotmail.com>
2020-05-28 17:14:02 +0100

    removed no docs

--- vagrant_rvm.sh
+++ vagrant_rvm.sh
@@ -9,5 +9,5 @@ rvm user gemsets
 rvm gemset use ruby-2.5.3@app_gemset --create
 
 gem install bundler
-gem install rails -v 5.2.1 --no-ri --no-rdoc
-gem install webpacker -v 3.5 --no-ri --no-rdoc
+gem install rails -v 5.2.1
+gem install webpacker -v 3.5
